5da63e0ca508a81bb366490d13ad810286a12e86
wassim4002 <52870003+wassim4002@users.noreply.github.com>
2019-07-15 01:06:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Your bot token here', 
+    TOKEN: 'Mzg4NDA5NjY0MTA0MzAwNTY2.XSvDPg.mkM5_TQPbZyrl0z-6unKSILqesA', 
     YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
-    devs: ['your id/developers here']
+    prefix: '-',
+    devs: ['388074664649293836']
 }

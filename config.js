ffd4c981eaa7593eea9e2df774738800e3ef436c
wassim4002 <52870003+wassim4002@users.noreply.github.com>
2019-07-15 01:21:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     TOKEN: 'Mzg4NDA5NjY0MTA0MzAwNTY2.XSvDPg.mkM5_TQPbZyrl0z-6unKSILqesA', 
-    YT_API_KEY: 'Youtube API Key here', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
     prefix: '-',
     devs: ['388074664649293836']
 }
